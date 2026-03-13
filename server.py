75d6776d82d6b0f0d975457c6b1c132ece2ea3d4
oxzee-dev <oxzeedev@gmail.com>
2026-03-13 02:00:10 +0100

    Update server.py

--- server.py
+++ server.py
@@ -268,4 +268,5 @@ def get_sec_filings(symbol: str) -> dict:
 # ─────────────────────────────────────────────────────────────
 if __name__ == "__main__":
     # mcp.run() # For Local run
-    mcp.run(transport="sse", host="0.0.0.0", port=8000) # for HTTP SSE
+    mcp.run(transport="sse", host="0.0.0.0", port=8000, path="/mcp") # for HTTP SSE MCP
+
